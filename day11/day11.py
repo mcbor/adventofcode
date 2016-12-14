7a6883f2da74ff41a1930e91c8389612218ea3c8
Martin Bor <github@sproenks.nl>
2016-12-14 01:13:36 +0000

    Rewrote day 11 to better prune the search space, and actually provide an answer.

--- day11/day11.py
+++ day11/day11.py
@@ -1,112 +1,80 @@
 #!/usr/bin/env python3
-# Advent of Code 2016 - Day 11, Part One
+# Advent of Code 2016 - Day 11, Part One & Two
 
-from pprint import pprint
 import sys
-from itertools import tee, filterfalse, chain, combinations
+from itertools import chain, combinations
 
+# test set
+# start = (1, ((2,1), (3,1)))
+# goal  = (4, tuple((4,4) for _ in range(len(start[1]))))
 
-start = (1, frozenset(['SG', 'PG', 'SM', 'PM']),        # Floor 1
-            frozenset(['TG', 'RG', 'CG', 'RM', 'CM']),  # Floor 2
-            frozenset(['TM']),                          # Floor 3
-            frozenset())                                # Floor 4
+# part one
+# start = (1, ((1,1), (1,1), (2,3), (2,2), (2,2)))
+# goal  = (4, tuple((4,4) for _ in range(len(start[1]))))
 
-goal = (4, frozenset(),                                                               # Floor 1
-           frozenset(),                                                               # Floor 2
-           frozenset(),                                                               # Floor 3
-           frozenset(['SG', 'PG', 'SM', 'PM', 'TG', 'RG', 'CG', 'RM', 'CM', 'TM']))   # Floor 4
-           
-           
-start2 = (1, frozenset(['EG','EM','DG','DM','SG', 'PG', 'SM', 'PM']),        # Floor 1
-            frozenset(['TG', 'RG', 'CG', 'RM', 'CM']),  # Floor 2
-            frozenset(['TM']),                          # Floor 3
-            frozenset())                                # Floor 4
+# part two
+start = (1, ((1,1), (1,1), (2,3), (2,2), (2,2), (1,1), (1, 1)))
+goal  = (4, tuple((4,4) for _ in range(len(start[1]))))
 
-goal2 = (4, frozenset(),                                                               # Floor 1
-           frozenset(),                                                               # Floor 2
-           frozenset(),                                                               # Floor 3
-           frozenset(['EG','EM','DG','DM','SG', 'PG', 'SM', 'PM', 'TG', 'RG', 'CG', 'RM', 'CM', 'TM']))   # Floor 4
+def tuple2list(state):
+    return [state[0], [list(i) for i in state[1]]]
+    
+def list2tuple(state):
+    return (state[0], tuple(tuple(i) for i in state[1]))
 
-           
-test_start = (1, frozenset(['HM', 'LM']),
-                 frozenset(['HG']),
-                 frozenset(['LG']),
-                 frozenset())
-                 
-test_goal = (4, frozenset(),
-                frozenset(),
-                frozenset(),
-                frozenset(['HG', 'HM', 'LG', 'LM']))
-                
-                
-def state_string(state):
-    out = []
-    for i in range(1,5):
-        out.append('F' + str(i) + ': ')
-        if state[0] == i:
-            out.append('E ')
-        for e in state[i]:
-            out.append(e + ' ')
-        out.append('\n')
-    return "".join(out)
-
-
-def partition(pred, iterable):
-    t1, t2 = tee(iterable)
-    return filterfalse(pred, t1), filter(pred, t2)
-
+def skey(state):
+    return (state[0], tuple(sorted(state[1])))
+    
+def flatten(state):
+    return [state[0]] + list(chain.from_iterable(state[1]))
 
 def is_valid(state):
-    if state[0] > 4 or state[0] < 1:
+    if any(filter(lambda x: x < 1 or x > 4, flatten(state))):
         return False
-    for floor in state[1:]:
-        generators, microchips = partition(lambda x: x[1] == 'M', floor)
-        generators = set([g[0] for g in generators])
-        microchips = set([m[0] for m in microchips])
-        if len(generators) == 0 or len(microchips) == 0:
-            # only generators or microchips on this floor
-            continue
-        if not microchips.issubset(generators):
-            # some microchips don't have a corresponding generator, so they're fried
+    generators, microchips = zip(*state[1])
+    for i, microchip in enumerate(microchips):
+        if generators[i] != microchip and microchip in generators:
             return False
     return True
 
+def gen_states(state):
+    current_floor = state[0]
 
-def gen_states(state, next):
-    current = state[0]
-    if next > 4 or next < 1:
-        return
-    moves = chain.from_iterable(combinations(state[current], r) for r in range(1, 3))
-    for move in moves:
-        new_state = list(state)
-        new_state[0] = next
-        new_state[current] -= set(move)
-        new_state[next] |= set(move)
-        if is_valid(new_state):
-            yield tuple(new_state)
-
-
-def next_states(state):
-    states = chain(gen_states(state, state[0] + 1), gen_states(state, state[0] - 1)) 
-    return set(states)
+    # candidates will be 2-tuple values, with (pair, kind), 
+    # whereby kind is 0 for the generator and 1 for the microchip
+    candidates = []
+    for kind, items in enumerate(zip(*state[1])):
+        candidates += [(pair, kind) for pair, floor in enumerate(items) if floor == current_floor]
+    combo = list(chain.from_iterable(combinations(candidates, r) for r in range(1, 3)))
+    for delta in [-1, 1]:
+        new_floor = current_floor + delta
+        if new_floor < 1 or new_floor > 4:
+            continue
+        for c in combo:
+            items = tuple2list(state)[1]
+            for move in c:
+                items[move[0]][move[1]] += delta
+            if is_valid([new_floor, items]):
+                yield list2tuple([new_floor, items])
+        
 
 def bfs_paths(start, goal):
     iterations = 0
     queue = [(start, [start])]
-    visited = set()
+    visited = set(skey(start))
     while queue:
         (vertex, path) = queue.pop(0)
-        for next in next_states(vertex) - set(path):
+        for current in set(gen_states(vertex)) - set(path):
             iterations += 1
             sys.stderr.write('{} {}\r'.format(iterations, len(path)))
-            if next == goal:
+            if current == goal:
                 sys.stderr.write('\n')
-                yield path + [next]
-            elif next in visited:
+                yield path + [current]
+            elif skey(current) in visited:
                 continue
             else:
-                visited.add(next)
-                queue.append((next, path + [next]))
+                visited.add(skey(current))
+                queue.append((current, path + [current]))
                 
 def shortest_path(start, goal):
     try:
@@ -116,9 +84,6 @@ def shortest_path(start, goal):
 
 def main(argv):
     path = shortest_path(start, goal)
-    for step, state in enumerate(path):
-        print('Step', step )
-        print(state_string(state))
     print(len(path) - 1)
     return 0
 

a093a8054cce9ef73e9f02e3395e606179a7ab57
Martin Bor <github@sproenks.nl>
2017-12-03 15:21:45 +0000

    Solution for day 3.

---
+++ 2017/03a.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+    03a.py
+    ~~~~~~
+    Advent of Code 2017 - Day 3: Spiral Memory
+    Part One
+
+    You come across an experimental new kind of memory stored on an infinite
+    two-dimensional grid.
+
+    Each square on the grid is allocated in a spiral pattern starting at a
+    location marked 1 and then counting up while spiraling outward. For
+    example, the first few squares are allocated like this:
+
+    17  16  15  14  13
+    18   5   4   3  12
+    19   6   1   2  11
+    20   7   8   9  10
+    21  22  23---> ...
+
+    While this is very space-efficient (no squares are skipped), requested data
+    must be carried back to square 1 (the location of the only access port for
+    this memory system) by programs that can only move up, down, left, or
+    right. They always take the shortest path: the Manhattan Distance between
+    the location of the data and square 1.
+
+    For example:
+
+    - Data from square 1 is carried 0 steps, since it's at the access port.
+    - Data from square 12 is carried 3 steps, such as: down, left, left.
+    - Data from square 23 is carried only 2 steps: up twice.
+    - Data from square 1024 must be carried 31 steps.
+
+    How many steps are required to carry the data from the square identified in
+    your puzzle input all the way to the access port?
+
+    :copyright: (c) 2017 by Martin Bor.
+    :license: MIT, see LICENSE for more details.
+"""
+
+import sys
+from math import ceil, sqrt
+
+
+def manhattan(p, q):
+    """Return manhattan distance between two points
+    
+    :p: 2-tuple
+    :q: 2-tuple
+    :returns: Manhattan distance between p and q"""
+    return int(abs(p[0] - q[0]) + abs(p[1] - q[1]))
+
+
+def spiral(n):
+    """Return Carthesian coordinates of for point n on a spiral
+    
+    :n: spiral point
+    :returns: Carthesian coordinate
+
+    >>> spiral(1)
+    (0, 0)
+    >>> spiral(7)
+    (-1, -1)
+    >>> spiral(25)
+    (2, -2)
+    >>> spiral(-1)
+    Traceback (most recent call last):
+      File "<stdin>", line 1, in <module>
+      File "<stdin>", line 8, in spiral
+    ValueError: math domain error
+    """
+    k = int(ceil((sqrt(n) - 1) / 2))
+    t = 2 * k + 1
+    m = t**2
+    t = t - 1
+    if n >= m - t:
+        return k - (m - n), -k
+    m = m - t
+    if n >= m - t:
+        return -k, -k + (m - n)
+    m = m - t
+    if n >= m - t:
+        return -k + (m - n), k
+    return k, k - (m - n - t)
+
+
+def solve(n):
+    """Calculate distance between square and the access port.
+
+    :input: data square
+    :return: distance to the access port
+
+    >>> solve(1)
+    0
+    >>> solve(12)
+    3
+    >>> solve(23)
+    2
+    >>> solve(1024)
+    31
+    """
+    return manhattan((0, 0), spiral(n))
+
+
+def main(argv):
+    if len(argv) == 2:
+        print(solve(int(argv[1])))
+    else:
+        print("Usage: {} SQUARE".format(argv[0]))
+
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv))
---
+++ 2017/03b.py
@@ -0,0 +1,132 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+    03b.py
+    ~~~~~~
+    Advent of Code 2017 - Day 3: Spiral Memory
+    Part Two
+
+    As a stress test on the system, the programs here clear the grid and then
+    store the value 1 in square 1. Then, in the same allocation order as shown
+    above, they store the sum of the values in all adjacent squares, including
+    diagonals.
+
+    So, the first few squares' values are chosen as follows:
+
+    - Square 1 starts with the value 1.
+    - Square 2 has only one adjacent filled square (with value 1), so it also
+      stores 1.
+    - Square 3 has both of the above squares as neighbors and stores the sum of
+      their values, 2.
+    - Square 4 has all three of the aforementioned squares as neighbors and
+     stores the sum of their values, 4.
+    - Square 5 only has the first and fourth squares as neighbors, so it gets
+      the value 5.
+
+    Once a square is written, its value does not change. Therefore, the first
+    few squares would receive the following values:
+
+    147  142  133  122   59
+    304    5    4    2   57
+    330   10    1    1   54
+    351   11   23   25   26
+    362  747  806--->   ...
+
+    What is the first value written that is larger than your puzzle input?
+
+    :copyright: (c) 2017 by Martin Bor.
+    :license: MIT, see LICENSE for more details.
+"""
+
+import sys
+from itertools import count, product
+from math import ceil, sqrt
+
+
+def spiral(n):
+    """Return Carthesian coordinates of for point n on a spiral
+    
+    :n: spiral point
+    :returns: Carthesian coordinate
+
+    >>> spiral(1)
+    (0, 0)
+    >>> spiral(7)
+    (-1, -1)
+    >>> spiral(25)
+    (2, -2)
+    >>> spiral(-1)
+    Traceback (most recent call last):
+      File "<stdin>", line 1, in <module>
+      File "<stdin>", line 8, in spiral
+    ValueError: math domain error
+    """
+    k = int(ceil((sqrt(n) - 1) / 2))
+    t = 2 * k + 1
+    m = t**2
+    t = t - 1
+    if n >= m - t:
+        return k - (m - n), -k
+    m = m - t
+    if n >= m - t:
+        return -k, -k + (m - n)
+    m = m - t
+    if n >= m - t:
+        return -k + (m - n), k
+    return k, k - (m - n - t)
+
+
+def fill():
+    """Fill the spiral memory, yielding each new value.
+
+    >>> f = fill()
+    >>> next(f)
+    1
+    >>> next(f)
+    1
+    >>> next(f)
+    2
+    >>> [next(f) for _ in range(10)]
+    [4, 5, 10, 11, 23, 25, 26, 54, 57, 59]
+    >>> [next(f) for _ in range(10)]
+    [122, 133, 142, 147, 304, 330, 351, 362, 747, 806]
+    """
+    memory = {(0, 0): 1}
+    neighbors = list(product((-1, 0, 1), repeat=2))
+    for n in count(1):
+        x, y = spiral(n)
+        coord = [(x + nb[0], y + nb[1]) for nb in neighbors]
+        val = sum(map(lambda v: memory.get(v, 0), coord))
+        memory[(x, y)] = val
+        yield val
+
+
+def solve(limit):
+    """Calculate distance between square and the access port.
+
+    :input: limit
+    :return: last value written
+
+    >>> solve(1)
+    2
+    >>> solve(12)
+    23
+    >>> solve(23)
+    25
+    >>> solve(1024)
+    1968
+    """
+    for n in fill():
+        if n > limit:
+            return n
+
+
+def main(argv):
+    if len(argv) == 2:
+        print(solve(int(argv[1])))
+    else:
+        print("Usage: {} LIMIT".format(argv[0]))
+
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv))

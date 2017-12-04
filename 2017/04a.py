cf18c4d6dddf41a995229028430c85c315b0e2b3
Martin Bor <github@sproenks.nl>
2017-12-04 09:44:56 +0000

    Added solution for day 4.

---
+++ 2017/04a.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+    04a.py
+    ~~~~~~
+    Advent of Code 2017 - Day 4: High-Entropy Passphrases
+    Part One
+
+    A new system policy has been put in place that requires all accounts to use
+    a passphrase instead of simply a password. A passphrase consists of a
+    series of words (lowercase letters) separated by spaces. To ensure
+    security, a valid passphrase must contain no duplicate words.
+
+    For example:
+
+    - aa bb cc dd ee is valid.
+    - aa bb cc dd aa is not valid - the word aa appears more than once.
+    - aa bb cc dd aaa is valid - aa and aaa count as different words.
+
+    The system's full passphrase list is available as your puzzle input. How
+    many passphrases are valid?
+
+    :copyright: (c) 2017 by Martin Bor.
+    :license: MIT, see LICENSE for more details.
+"""
+
+import sys
+
+
+def solve(passphrases):
+    """Calculate number of valid passphrases.
+
+    :passphrases: string of passphrases, separated by newlines
+    :return: number of valid passphrases
+
+    >>> solve('aa bb cc dd ee')
+    1
+    >>> solve('aa bb cc dd aa')
+    0
+    >>> solve('aa bb cc dd aaa')
+    1
+    """
+    return sum(len(words) == len(set(words))
+               for words in (passphrase.split()
+                             for passphrase in passphrases.split('\n')))
+
+
+def main(argv):
+    if len(argv) == 2:
+        f = open(argv[1], 'r')
+    else:
+        sys.stderr.write('reading from stdin...\n')
+        f = sys.stdin
+    print(solve(f.read().strip()))
+
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv))
---
+++ 2017/04b.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+    04b.py
+    ~~~~~~
+    Advent of Code 2017 - Day 4: High-Entropy Passphrases
+    Part Two
+
+    For added security, yet another system policy has been put in place. Now, a
+    valid passphrase must contain no two words that are anagrams of each
+    other - that is, a passphrase is invalid if any word's letters can be 
+    rearranged to form any other word in the passphrase.
+
+    For example:
+
+    - 'abcde fghij' is a valid passphrase.
+    - 'abcde xyz ecdab' is not valid - the letters from the third word can be 
+      rearranged to form the first word.
+    - 'a ab abc abd abf abj' is a valid passphrase, because all letters need to 
+      be used when forming another word.
+    - 'iiii oiii ooii oooi oooo' is valid.
+    - 'oiii ioii iioi iiio' is not valid - any of these words can be rearranged 
+      to form any other word.
+
+    Under this new system policy, how many passphrases are valid?
+
+    :copyright: (c) 2017 by Martin Bor.
+    :license: MIT, see LICENSE for more details.
+"""
+
+import sys
+
+
+def solve(passphrases):
+    """Calculate number of valid passphrases.
+
+    :passphrases: string of passphrases, separated by newlines
+    :return: number of valid passphrases
+
+    >>> solve('abcde fghij')
+    1
+    >>> solve('abcde xyz ecdab')
+    0
+    >>> solve('a ab abc abd abf abj')
+    1
+    >>> solve('iiii oiii ooii oooi oooo')
+    1
+    >>> solve('oiii ioii iioi iiio')
+    0
+    """
+    valid = 0
+    for passphrase in passphrases.split('\n'):
+        words = passphrase.split()
+        words = [''.join(word) for word in map(sorted, words)]
+        if len(words) == len(set(words)):
+            valid += 1
+    return valid
+
+
+def main(argv):
+    if len(argv) == 2:
+        f = open(argv[1], 'r')
+    else:
+        sys.stderr.write('reading from stdin...\n')
+        f = sys.stdin
+    print(solve(f.read().strip()))
+
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv))
